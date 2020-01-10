94402f7b651038c177545f60c39b757bec9210dc
XiYan <1848293548@qq.com>
2020-01-10 12:02:44 +0800

    11.1 create project

---
+++ app/src/main/java/com/example/mycloudmusicandroidjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mycloudmusicandroidjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
